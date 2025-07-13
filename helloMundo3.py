eceb7ba89dc1cae88e82afec4c93d7379bf9c476
Administrdor <a06386936@gmail.com>
2025-07-13 14:02:46 -0600

    Correpción de errores, porque no lo habia guardado antes

--- helloMundo3.py
+++ helloMundo3.py
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-print("Hola mundo 3 cambio de prueba, este es otro cambio, mas cambios vLogin")
-=======
 print("Hola mundo 3 cambio de prueba, este es otro cambio, mas cambios v 3")
->>>>>>> main
+
